7c898501a240248e5b9745e0b564f92e28a0b116
Guarlic <hyunujung0419@gmail.com>
2024-04-13 14:22:50 +0900

    attack.rs 추가

---
+++ src/systems/attack.rs
@@ -0,0 +1,9 @@
+use bevy::prelude::*;
+
+pub fn spawn_preview() {
+    // 공격 미리보기 
+}
+
+pub fn spawn_attack() {
+    // 공격 발사
+}
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,5 +1,7 @@
 pub mod camera;
 pub mod player;
+pub mod attack;
 
 pub use camera::*;
 pub use player::*;
+pub use attack::*;
